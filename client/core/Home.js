033eddb43a95cf4596ec18885dc8825ff3b4edb7
Sam-Martin12334 <1804372@rgu.ac.uk>
2022-04-07 20:28:32 +0100

    Finished up to lab 6 part 5

--- client/core/Home.js
+++ client/core/Home.js
@@ -5,49 +5,48 @@ import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 import myImg from './../assets/images/space.jpg'
-
+import {Link} from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
-    card: {
+  card: {
     maxWidth: 600,
     margin: 'auto',
     marginTop: theme.spacing(5),
     marginBottom: theme.spacing(5)
-    },
-    title: {
+  },
+  title: {
     padding:`${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(2)}px`,
     color: theme.palette.openTitle
-    },
-    media: {
+  },
+  media: {
     minHeight: 400
-    },
-    credit: {
+  },
+  credit: {
     padding: 10,
     textAlign: 'right',
     backgroundColor: '#ededed',
     borderBottom: '1px solid #d0d0d0',
     '& a':{
-    color: '#3f4771'
+      color: '#3f4771'
     }
-    }
-   }))
+  }
+}))
 
-
-   export default function Home(){
-    const classes = useStyles()
+export default function Home(){
+  const classes = useStyles()
     return (
-    <Card className={classes.card}>
-    <Typography variant="h6" className={classes.title}>
-    Home Page
-    </Typography>
-    <CardMedia className={classes.media} image={myImg} title="My Image"/>
-    <Typography variant="body2" component="p" className={classes.credit}
-   color="textSecondary">Photo: Picasso</Typography>
-    <CardContent>
-    <Typography variant="body1" component="p">
-    Welcome to Lab 6 home page.
-    </Typography>
-    </CardContent>
-    </Card>
+        <Card className={classes.card}>
+          <Typography variant="h6" className={classes.title}>
+            Home Page
+          </Typography>
+          <Link to="/users"> Users </Link>
+          <CardMedia className={classes.media} image={myImg} title="My Image"/>
+          <Typography variant="body2" component="p" className={classes.credit} color="textSecondary">Photo: Chompster</Typography>
+          <CardContent>
+            <Typography variant="body1" component="p">
+              Welcome to Lab 6 home page.
+            </Typography>
+          </CardContent>
+        </Card>
     )
-   }
+}
