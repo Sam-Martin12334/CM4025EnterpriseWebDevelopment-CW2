98cea38fd9537cb421512036b89d3813fd909611
Sam-Martin12334 <1804372@rgu.ac.uk>
2022-04-07 18:54:12 +0100

    gotten to react home page

--- template.js
+++ template.js
@@ -1,13 +1,15 @@
 export default () => {
     return `<!doctype html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8">
-                <title> Lab Six </title>
-            </head>
-            <body style="margin:0">
-                <div id="root">Hello World</div>
-            </body>
-        </html>`
+    <html lang="en">
+    <head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <meta charset="utf-8">
+    <title> Lab Six </title>
+    </head>
+    <body style="margin:0">
+    <div id="root></div>
+    <script type="text/javascript" src="/dist/bundle.js"></script>
+    </body>
+    </html>`
    }
-   
