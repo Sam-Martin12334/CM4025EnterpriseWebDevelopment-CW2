bf2c6f3a89d4dc39c50325f47e9e82e73f30f0f3
Sam-Martin12334 <1804372@rgu.ac.uk>
2022-04-08 13:22:30 +0100

    Updated the home page to change from lab 6 to the Love for Uglies home page. I implemented a Image list to show off some animals, as well as adding a description

--- client/core/Home.js
+++ client/core/Home.js
@@ -4,22 +4,33 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
-import myImg from './../assets/images/space.jpg'
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import ImageListItemBar from '@material-ui/core/ImageListItemBar';
+import spiderImg from './../assets/images/spider.jpg'
+import scorpImg from './../assets/images/scorpion.jpg'
+import catImg from './../assets/images/cat.jpg'
+import walrusImg from './../assets/images/walrus.jpg'
 import {Link} from 'react-router-dom'
+
+
+
 
 const useStyles = makeStyles(theme => ({
   card: {
-    maxWidth: 600,
+    maxWidth: 900,
     margin: 'auto',
     marginTop: theme.spacing(5),
     marginBottom: theme.spacing(5)
   },
   title: {
     padding:`${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(2)}px`,
-    color: theme.palette.openTitle
+    color: theme.palette.openTitle,
+    textAlign: 'center',
   },
   media: {
-    minHeight: 400
+    height: 400,
+    width: 400
   },
   credit: {
     padding: 10,
@@ -32,21 +43,71 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
+
+
+const itemData = [
+  {
+    img: spiderImg,
+    title: 'Spider',
+    author: '@Michael Willinger',
+    link: 'https://www.pexels.com/photo/close-up-photo-of-spider-3482977/'
+  },
+  {
+    img: scorpImg,
+    title: 'Scorpion',
+    author: '@Sharath G.',
+    link: 'https://www.pexels.com/photo/black-and-brown-insect-with-pincers-1981542/'  
+  },
+  {
+    img: catImg,
+    title: 'Spyhnx Cat',
+    author: '@KoolShooters',
+    link: 'https://www.pexels.com/photo/a-blue-eyed-sphynx-cat-7680664/'  
+  },
+  {
+    img: walrusImg,
+    title: 'Walrus',
+    author: '@Putulik Jaaka',
+    link: 'https://www.pexels.com/photo/photo-of-walruses-with-white-tusks-3164496/'  
+  }
+];
+
 export default function Home(){
   const classes = useStyles()
     return (
+      <>
         <Card className={classes.card}>
-          <Typography variant="h6" className={classes.title}>
-            Home Page
+          <Typography variant="h4" className={classes.title}>
+            Welcome to the Love for the Uglies home page!
           </Typography>
-          <Link to="/users"> Users </Link>
-          <CardMedia className={classes.media} image={myImg} title="My Image"/>
-          <Typography variant="body2" component="p" className={classes.credit} color="textSecondary">Photo: Chompster</Typography>
           <CardContent>
-            <Typography variant="body1" component="p">
-              Welcome to Lab 6 home page.
-            </Typography>
+          <Typography variant="h6" className={classes.title}>
+            Here at the Love for the Uglies wesbite, we want to show off the beatiful animals that are considered "Ugly" by society. The images below are to show off some of our favourites!
+          </Typography>
+          <ImageList sx={{ width: 500, height: 450 }}>
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={item.title}
+            subtitle={<><span>by: {item.author}</span> <span>Link:  {item.link}</span></>}
+            
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
           </CardContent>
         </Card>
+        <Card className={classes.card}>
+        </Card>
+        </>
+
+        
+        
     )
 }
