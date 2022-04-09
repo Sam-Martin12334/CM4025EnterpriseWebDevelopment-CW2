84e9241b384a821eaac718fda2fd5f5f8a108fb8
Sam-Martin12334 <1804372@rgu.ac.uk>
2022-04-09 12:39:22 +0100

    same as above

--- server.cmd
+++ server.cmd
@@ -2,6 +2,7 @@
 set PORT=8080
 set JWT=mysecret
 set MONGODB_URI=mongodb+srv://admin:password1234@cluster0.wg0en.mongodb.net/users?retryWrites=true&w=majority
+set NODE_ENV=start
 npm run-script development
 
 
