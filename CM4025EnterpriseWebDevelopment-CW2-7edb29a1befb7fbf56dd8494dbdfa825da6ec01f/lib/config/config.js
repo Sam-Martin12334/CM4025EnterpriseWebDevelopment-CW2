750385c56702a781c34cce7878ca77d461a0a5ba
Sam-Martin12334 <1804372@rgu.ac.uk>
2022-04-11 14:39:08 +0100

    made mongo URI SET

--- CM4025EnterpriseWebDevelopment-CW2-7edb29a1befb7fbf56dd8494dbdfa825da6ec01f/lib/config/config.js
+++ CM4025EnterpriseWebDevelopment-CW2-7edb29a1befb7fbf56dd8494dbdfa825da6ec01f/lib/config/config.js
@@ -1,13 +1,10 @@
 
 
 const config = {
-    env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3000,
+    env: process.env.NODE_ENV || 'start',
+    port: process.env.PORT || 8000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-    mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || '127.0.0.1') + ':' +
-    (process.env.MONGO_PORT || '27017') + '/mydb/'
+    mongoUri: 'mongodb+srv://admin:password1234@cluster0.wg0en.mongodb.net/users?retryWrites=true&w=majority'
 
     
    }
