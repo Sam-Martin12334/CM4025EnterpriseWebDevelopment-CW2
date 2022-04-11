1480e0f928150848c94d1460a415910381e51d2c
Sam-Martin12334 <1804372@rgu.ac.uk>
2022-04-11 14:11:10 +0100

    adding a sh file for ubunut

---
+++ CM4025EnterpriseWebDevelopment-CW2-7edb29a1befb7fbf56dd8494dbdfa825da6ec01f/lib/server.sh
@@ -0,0 +1,8 @@
+@echo off
+set PORT=8080
+set JWT=mysecret
+set MONGODB_URI=mongodb+srv://admin:password1234@cluster0.wg0en.mongodb.net/users?retryWrites=true&w=majority
+set NODE_ENV=start
+npm run-script development
+
+
